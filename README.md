5530bfaa1871b09944de282d27aacb6d442ea520
Jacob Meneses <jaykobyou@gmail.com>
2019-04-11 11:12:21 -0500

    Updating readme and initial test data on test database

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Loopback 4 Exercise
 
 [![LoopBack](https://github.com/strongloop/loopback-next/raw/master/docs/site/imgs/branding/Powered-by-LoopBack-Badge-(blue)-@2x.png)](http://loopback.io/)
+
+## Example of endpoint calls 
+
+```bash
+curl http://127.0.0.1:3000/shops
+curl http://127.0.0.1:3000/shops/1/products
+```
---
+++ inserts.sql
@@ -0,0 +1,11 @@
+
+
+INSERT INTO "shop" ("id", "name", "description") VALUES
+(1,	'restaurant two',	'restaurant two'),
+(2,	'restaurant one',	'restaurant one'),
+(3,	'restaurant THREE',	'restaurant THREE');
+
+
+INSERT INTO "product" ("id", "name", "description", "score", "shop_id") VALUES
+(1,	'rice',	'rice with meatballs',	4.7,	1),
+(2,	'hamburger',	'hamburger with cheese',	4.8,	2);
